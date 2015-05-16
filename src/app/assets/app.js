2d57701eda0a164521b7742b176e31482412da2c
PeterBernhardt <peter.bernhardt@gmail.com>
2015-05-16 04:58:20 +0200

    removed local Base64 encoder and replaced with community library

--- src/app/assets/app.js
+++ src/app/assets/app.js
@@ -387,71 +387,6 @@
             return array;
         }
 
-        function base64decode(input) {
-            function _utf8Decode(utftext) {
-                var string = "";
-                var i = 0;
-                var c = 0;
-                var c2  = 0;
-
-                while (i < utftext.length) {
-
-                    c = utftext.charCodeAt(i);
-
-                    if (c < 128) {
-                        string += String.fromCharCode(c);
-                        i++;
-                    }
-                    else if ((c > 191) && (c < 224)) {
-                        c2 = utftext.charCodeAt(i + 1);
-                        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                        i += 2;
-                    }
-                    else {
-                        c2 = utftext.charCodeAt(i + 1);
-                        var c3 = utftext.charCodeAt(i + 2);
-                        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                        i += 3;
-                    }
-
-                }
-
-                return string;
-            }
-
-            var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-            var output = "";
-            var chr1, chr2, chr3;
-            var enc1, enc2, enc3, enc4;
-            var i = 0;
-
-            input = input.replace(/[^A-Za-z0-9\+\/=]/g, "");
-
-            while (i < input.length) {
-
-                enc1 = _keyStr.indexOf(input.charAt(i++));
-                enc2 = _keyStr.indexOf(input.charAt(i++));
-                enc3 = _keyStr.indexOf(input.charAt(i++));
-                enc4 = _keyStr.indexOf(input.charAt(i++));
-
-                chr1 = (enc1 << 2) | (enc2 >> 4);
-                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                chr3 = ((enc3 & 3) << 6) | enc4;
-
-                output = output + String.fromCharCode(chr1);
-
-                if (enc3 != 64) {
-                    output = output + String.fromCharCode(chr2);
-                }
-                if (enc4 != 64) {
-                    output = output + String.fromCharCode(chr3);
-                }
-            }
-            output = _utf8Decode(output);
-            return output;
-        }
-
         function isUndefinedOrNull(val) {
             return angular.isUndefined(val) || val === null;
         }
@@ -464,7 +399,6 @@
             $timeout: $timeout,
             // generic
             activateController: activateController,
-            base64decode: base64decode,
             changePatientList: changePatientList,
             changePersonList: changePersonList,
             changePractitionerList: changePractitionerList,
